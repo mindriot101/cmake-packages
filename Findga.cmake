e9d08a400120638404f93f6681b2fec13b007964
Simon Walker <s.r.walker101@googlemail.com>
2011-04-16 13:07:30 +0100

    added GA cmake file
    for Genetic Algorithms

---
+++ Findga.cmake
@@ -0,0 +1,25 @@
+FIND_PATH(GA_INCLUDE_DIR ga.h PATHS 
+    $ENV{HOME}/build/include
+    /opt/local/include
+    PATH_SUFFIXES
+    ga
+    )
+
+FIND_LIBRARY(GA_LIBRARIES NAMES ga PATHS
+    $ENV{HOME}/build/lib
+    /opt/local/lib
+    )
+
+if (GA_INCLUDE_DIR AND GA_LIBRARIES)
+    set (GA_FOUND true)
+endif (GA_INCLUDE_DIR AND GA_LIBRARIES)
+
+if (GA_FOUND)
+    if (NOT GA_FIND_QUIETLY)
+        message(STATUS "Found ga: ${GA_LIBRARIES}")
+    endif(NOT GA_FIND_QUIETLY)
+else (GA_FOUND)
+   if (GA_FIND_REQUIRED)
+      message(FATAL_ERROR "Could not find ga")
+    endif(GA_FIND_REQUIRED)
+endif(GA_FOUND) 
