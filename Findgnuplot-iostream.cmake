4fc1c383db7b5e7964805084d851998535e42235
Simon Walker <s.r.walker101@googlemail.com>
2011-05-16 12:06:54 +0100

    Added gnuplot-iostream finder

---
+++ Findgnuplot-iostream.cmake
@@ -0,0 +1,39 @@
+# Set up your library names here
+SET(LIBRARY GNUPLOT_IOS)
+SET(LIBRARYNAME Gnuplot-iostream)
+FIND_PACKAGE(Boost COMPONENTS iostreams REQUIRED)
+FIND_PATH(${LIBRARY}_INCLUDE_DIR gnuplot-iostream.h # insert the header file name here
+    PATHS 
+    /opt/local/include
+    $ENV{HOME}/build/include
+    PATH_SUFFIXES
+    gnuplot-iostream
+    )
+
+#FIND_LIBRARY(${LIBRARY}_LIBRARIES 
+    #NAMES # insert the library name here (without the lib...) 
+    #PATHS
+    #$ENV{HOME}/build/lib
+    #/opt/local/lib
+    #/usr/lib64
+    #)
+
+set(${LIBRARY}_LIBRARIES ${Boost_LIBRARIES})
+set(${LIBRARY}_INCLUDE_DIR ${LIBRARY}_INCLUDE_DIR ${Boost_INCLUDE_DIR})
+
+
+
+if (${LIBRARY}_INCLUDE_DIR AND ${LIBRARY}_LIBRARIES)# AND ${LIBRARY}_LIBRARIES)
+    set (${LIBRARY}_FOUND true)
+endif(${LIBRARY}_INCLUDE_DIR AND ${LIBRARY}_LIBRARIES)# AND ${LIBRARY}_LIBRARIES)
+
+
+if (${LIBRARY}_FOUND)
+    if (NOT ${LIBRARY}_FIND_QUIETLY)
+        message(STATUS "Found ${LIBRARYNAME}: ${${LIBRARY}_LIBRARIES}")
+    endif(NOT ${LIBRARY}_FIND_QUIETLY)
+else (${LIBRARY}_FOUND)
+   if (${LIBRARY}_FIND_REQUIRED)
+      message(FATAL_ERROR "Could not find ${LIBRARYNAME}")
+    endif(${LIBRARY}_FIND_REQUIRED)
+endif(${LIBRARY}_FOUND) 
